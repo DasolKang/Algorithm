7f741ffcc6645f962f4753b25bcd12c1ba433130
DasolKang <dasol199@naver.com>
2023-01-19 09:45:57 +0900

    :zap: change wrong file

--- Baekjoon/baekjoon_27210.py
+++ Baekjoon/baekjoon_27210.py
@@ -1,37 +1,24 @@
-# 도넛처럼 연결된 행성의 구역의 수를 구해라
-# 0에서 왼쪽으로 이동 시 (0, M-1) 위로 이동시 (N-1, 0)
-# 0은 비어있는 것이며 1은 숲으로 막혀있는 것이다
-
 import sys
-from collections import deque
 
 input=sys.stdin.readline
-dx=[-1,1,0,0]
-dy=[0,0,-1,1]
-N, M=map(int,input().split())
-board=[list(map(int,input().split())) for _ in range(N)]
-visit=[[False]*M for _ in range(N)]
-
-def bfs(i, j):
-    queue=deque()
-    queue.append([i,j])
-    while (queue):
-        x, y = queue.popleft()
-        for d in range(4):
-            nx = (x+dx[d])%N
-            ny = (y+dy[d])%M
-            if (board[nx][ny]==0 and not visit[nx][ny]):
-                visit[nx][ny]=True
-                queue.append([nx,ny])
-
-zero=[]
+N=int(input())
+array=list(map(int,input().split()))
 result=0
-for i in range(N):
-    for j in range(M):
-        if (board[i][j]==0):
-            if (not visit[i][j]):
-                visit[i][j]=True
-                result+=1
-                bfs(i, j)
-
+start=0
+cur=array[0]; nextIs=0; isChange=False
+while (start<N):
+    left=0; right=0; aha=0
+    for end in range(start, N):
+        if (not isChange and array[end]!=cur): 
+            isChange=True
+            nextIs=end
+        if (array[end]==1): left+=1
+        elif (array[end]==2): right+=1
+        aha=abs(left-right)
+        if (aha<1): break
+        if (result<aha): result=aha
+    if (not isChange): break
+    start=nextIs
+    cur=array[nextIs]
+    isChange=False
 print(result)
