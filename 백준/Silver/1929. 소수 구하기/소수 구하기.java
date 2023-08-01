ed503bd223807b42df1f421841d851ac00925f0f
DASOL <66811034+DasolKang@users.noreply.github.com>
2023-08-02 08:39:25 +0900

    [Silver III] Title: 소수 구하기, Time: 1072 ms, Memory: 36896 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int M = scanner.nextInt(), N =scanner.nextInt();
+
+        int[] isPrime = new int[N+1];
+        Arrays.fill(isPrime, 1);
+        isPrime[0]=1; isPrime[1]=0;
+        for (int i=2;i<N+1;i++) {
+            for (int j=2;i*j<N+1;j++) {
+                isPrime[i*j]=0;
+            }
+        }
+
+        for (int i=M;i<=N;i++) {
+            if (isPrime[i]==1) System.out.println(i);
+        }   
+    }
+}
