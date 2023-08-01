5bef0eefda013fbe7bee93076a86da47b28f2400
DASOL <66811034+DasolKang@users.noreply.github.com>
2023-08-01 10:47:18 +0900

    [Bronze II] Title: 소수 찾기, Time: 132 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -7,24 +7,21 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		StringTokenizer tokenizer = new StringTokenizer(br.readLine());
-		int[] number = new int[N];
-		boolean[] isPrime = getPrime();
 		int answer = 0;
 		for (int i=0;i<N;i++) {
-			if (isPrime[Integer.parseInt(tokenizer.nextToken())]) answer+=1;
+			int num = Integer.parseInt(tokenizer.nextToken());
+			if (num<2) continue;
+			if (num==2) {
+				answer+=1;
+				continue;
+			}
+			boolean isPrime=true;
+			for (int j=2;j<(int)Math.pow(num, 0.5)+1;j++) {
+				if (num%j==0) isPrime=false; 
+			}
+			if (isPrime) answer++;
 		}
 		System.out.println(answer);
 	}
 	
-	static boolean[] getPrime() {
-		boolean[] result = new boolean[1001];
-		Arrays.fill(result, true);
-		result[0]=false; result[1]=false;
-		for (int i=2;i<(int)Math.pow(1001, 0.5);i++) {
-			for (int j=i*2;j<1001;j+=i) {
-				result[j]=false;
-			}
-		}
-		return result;
-	}
 }
