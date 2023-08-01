dcbd66bf8af3df0758940bc3983aa5a8a5ddd022
DASOL <66811034+DasolKang@users.noreply.github.com>
2023-08-01 17:39:22 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 144 ms, Memory: 15680 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,65 +1,52 @@
-import java.util.*;
-import java.io.*;
-/*
- * 1. summary: 문제 해석
- * 		남학생 : 자신의 번호의 배수 번호들의 스위치 상태 변경
- * 		여학생 : 자신의 번호 기준 좌우대칭&같은 상태인 스위치 상태 변경
- * 
- * 2. strategy: 풀이전략
- * 3. note: 주의할 사항(특이사항)
- * 		스위치 정보는 한 줄에 20개씩 출력한다
- * 		21번째 스위치는 두번째줄에 출력
- */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	static int[] switches;
-	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		StringTokenizer tokenizer= new StringTokenizer(br.readLine());
-		switches = new int[N];
-		for (int i=0;i<N;i++) {
-			switches[i]= Integer.parseInt(tokenizer.nextToken());
+		int swcnt = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] state = new int[swcnt+1];
+		for (int i = 1; i <= swcnt; i++) {
+			state[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		int studentNum = Integer.parseInt(br.readLine());
-		for (int i=0;i<studentNum;i++) {
-			tokenizer = new StringTokenizer(br.readLine());
-			int gender = Integer.parseInt(tokenizer.nextToken());
-			int switchNum = Integer.parseInt(tokenizer.nextToken());
-			pushSwitch(N, gender, switchNum);
-		}
-		
-		for(int i=1;i<N+1;i++) {
-			System.out.print(switches[i-1]+" ");
-			if (i%20==0) System.out.println();
-		}
-		
-	}
-	
-	static void pushSwitch(int N, int gender, int switchNum) {
-		if (gender==1) {
-			// 남학생 -> 자신 + 자신의 배수 스위치 상태 변경
-			for (int i=switchNum-1;i<N;i+=switchNum) {
-				switches[i]=(switches[i]+1)%2;
-			}
-		} else if (gender==2) {
-			// 여학생 -> 자신 기준 좌우대칭&값이 같은 스위치 상태 변경
-			switchNum--;
-			switches[switchNum] = (switches[switchNum]+1)%2;
-			for (int i=1;i<=switchNum;i++) {
-				if (isPoss(switchNum, i, N)) {
-					switches[switchNum-i] = (switches[switchNum-i]+1)%2;
-					switches[switchNum+i] = (switches[switchNum+i]+1)%2;
-				} else break;
+		int stcnt = Integer.parseInt(br.readLine());
+		for (int i = 0; i < stcnt; i++) {
+			st = new StringTokenizer(br.readLine());
+			int stud = Integer.parseInt(st.nextToken());	// 학생 성별
+			int num = Integer.parseInt(st.nextToken());
+			switch (stud) {
+				case 1:
+					for (int j = num; j <= swcnt; j+=num) {
+						state[j] = ~state[j] + 2;
+					}
+					break;
+				case 2:
+					int ln = num - 1, rn = num + 1;
+					state[num] = ~state[num] + 2;
+					while (ln > 0 && rn <= swcnt && state[ln] == state[rn]) {
+						state[ln] = ~state[ln] + 2;
+						state[rn] = ~state[rn] + 2;
+						ln --;
+						rn ++;
+					}
+					break;
 			}
 		}
-	}
-	
-	static boolean isPoss(int switchNum, int i, int N) {
-		return switchNum-i>=0 && switchNum+i<N && switches[switchNum-i]==switches[switchNum+i];
+		
+		int idx = 1;
+		while (idx <= swcnt) {
+			if (idx > 1 && idx % 20 == 1) {
+				System.out.println();
+			}
+			System.out.print(state[idx] + " ");
+			idx++;
+		}
+				
 	}
 
 }
