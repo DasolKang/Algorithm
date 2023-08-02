4d8c0aed16b23929b02ecbda0bf435b639dac1b6
DASOL <66811034+DasolKang@users.noreply.github.com>
2023-08-02 14:45:24 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1708 ms, Memory: 128908 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		
+		int[][] seq = new int[n+1][n+1];
+		int[][] prefix = new int[n+1][n+1];
+		for (int i = 1; i < n+1; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j < n+1; j++) {
+				seq[j][i] = Integer.parseInt(st.nextToken());
+				prefix[j][i] = prefix[j][i-1] + prefix[j-1][i] - prefix[j-1][i-1] + seq[j][i];
+			}
+		}
+		
+		for (int k = 0; k < m; k++) {
+			st = new StringTokenizer(br.readLine());
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+			
+			System.out.println(prefix[y2][x2] - prefix[y1-1][x2] - prefix[y2][x1-1] + prefix[y1-1][x1-1]);
+		}
+	}
+
+}
