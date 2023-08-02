ecc98c1e26120659edc05ed1ae6d2ec35397f1c5
DASOL <66811034+DasolKang@users.noreply.github.com>
2023-08-02 12:10:50 +0900

    [Silver III] Title: N과 M (1), Time: 1940 ms, Memory: 51652 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,35 +1,39 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
+	
+	private static int n, m;
+	private static int[] seq;
+	private static boolean[] isSelected;
 
-	static int N, M, answer[];
-	static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) throws IOException {
-		Scanner scanner = new Scanner(System.in);
-		N = scanner.nextInt();
-		M = scanner.nextInt();
-		answer = new int[M];
-		permutation(0, new boolean[N+1]);
-		System.out.print(sb.toString());
+	public static void main(String[] args) throws Exception{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		n = Integer.parseInt(input.split(" ")[0]);
+		m = Integer.parseInt(input.split(" ")[1]);
+		seq = new int[m];
+		isSelected = new boolean[n+1];
+		getSeq(0);
 	}
 	
-	public static void permutation(int index, boolean[] visited) {
-		if (index == M) {
-			for (int i=0;i<M;i++) {
-				sb.append(answer[i]+" ");
+	private static void getSeq(int idx) {
+		if (idx == m) {
+			for (int i = 0; i < m; i++) {
+				System.out.print(seq[i] + " ");
 			}
-			sb.append("\n");
-			return ;
+			System.out.println();
+			return;
 		}
-		for (int i=1;i<=N;i++) {
-			if (! visited[i]) {
-				answer[index]=i;
-				visited[i]=true;
-				permutation(index+1, visited);
-				visited[i]=false;
-			}
+		
+		for (int i = 1; i <= n; i++) {
+			if (isSelected[i]) continue;
+			isSelected[i] = true;
+			seq[idx] = i;
+			getSeq(idx+1);
+			isSelected[i] = false;
 		}
 	}
+
 }
