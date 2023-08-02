07823d7ca18724afc74be19d2518494578549d45
DASOL <66811034+DasolKang@users.noreply.github.com>
2023-08-02 14:23:18 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1484 ms, Memory: 58472 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,45 +1,31 @@
-import java.util.*;
-import java.io.*;
-
-/*
- * 1. summary: 문제 해석
- * 	  수 N개가 주어졌을 때 i번~j번째 수의 합을 구하라
- * 2. strategy: 풀이전략
- *    0번 인덱스부터 본인의 인덱스까지의 합을 구한 후
- *    i~j의 합이 [0~j]의 합 - [0~i-1]의 합으로 구할 수 있다는 점을 이용 
- * 3. note: 주의할 사항(특이사항)
- */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer tokenizer = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(tokenizer.nextToken());
-		int M = Integer.parseInt(tokenizer.nextToken());
-		int[] number = new int[N];
-		tokenizer = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			number[i] = Integer.parseInt(tokenizer.nextToken());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		int[] seq = new int[n+1];
+		int[] prefix = new int[n+1];
+		for (int i = 1; i < n+1; i++) {
+			seq[i] = Integer.parseInt(st.nextToken());
+			prefix[i] = prefix[i-1] + seq[i];
 		}
-		int[] prefixSum = getPrefixSum(number);
-		for (int t = 0; t < M; t++) {
-			tokenizer = new StringTokenizer(br.readLine());
-			int i = Integer.parseInt(tokenizer.nextToken());
-			int j = Integer.parseInt(tokenizer.nextToken());
-			System.out.println(prefixSum[j]-prefixSum[i-1]);
+		
+		for (int k = 0; k < m; k++) {
+			st = new StringTokenizer(br.readLine());
+			int i = Integer.parseInt(st.nextToken());
+			int j = Integer.parseInt(st.nextToken());
+			
+			System.out.println(prefix[j] - prefix[i-1]);
 		}
-
-	}
-
-	static int[] getPrefixSum(int[] array) {
-		int sum = 0;
-		int[] result = new int[array.length+1];
-		for (int i = 1; i <= array.length; i++) {
-			sum += array[i-1];
-			result[i] = sum;
-		}
-		return result;
 	}
 
 }
