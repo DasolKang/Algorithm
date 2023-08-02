65142b68b3b27dbc83562cf38728c93a9ea0776a
DASOL <66811034+DasolKang@users.noreply.github.com>
2023-08-02 13:02:19 +0900

    [Silver III] Title: N과 M (2), Time: 236 ms, Memory: 18336 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+	static int N, M, answer[];
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner(System.in);
+		N = scanner.nextInt();
+		M = scanner.nextInt();
+		answer = new int[M];
+		permutation(0, new boolean[N + 1]);
+		System.out.print(sb.toString());
+	}
+
+	public static void permutation(int index, boolean[] visited) {
+		if (index == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(answer[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for (int i = 1; i <= N; i++) {
+			if (!visited[i]) {
+				if (index == 0 || (index!=0 && answer[index-1]<i)) {
+					answer[index] = i;
+					visited[i] = true;
+					permutation(index + 1, visited);
+					visited[i] = false;
+				}
+			}
+		}
+	}
+}
