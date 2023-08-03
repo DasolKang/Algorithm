4002f629c8943d5e1c132c62303f85884f2fbe99
DASOL <66811034+DasolKang@users.noreply.github.com>
2023-08-03 13:37:14 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 132 ms, Memory: 14432 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -1,53 +1,41 @@
-import java.util.*;
-import java.io.*;
-
-/*
- * 1. summary: 문제 해석
- * 	  N개의 재료는 각각 쓴맛과 신맛의 정도를 가지고 있다.
- * 	  신맛은 사용한 재료의 곱이며 쓴맛은 사용한 재료의 합이다.
- * 	  재료를 적절하게 사용하여 신맛과 쓴맛의 차이가 가장 적은 요리를 만들어라.
- * 2. strategy: 풀이전략
- * 	  재료를 뽑는 모든 경우의 수의 신맛과 쓴맛의 차이를 구한다.
- * 3. note: 주의할 사항(특이사항)
- */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
+	
+	private static int n, ingredient[][], diff;
+	private static boolean[] set;
 
-	static int[] sourTaste;
-	static int[] badTaste;
-	static int N;
-	static int minDiff;
-
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		sourTaste = new int[N];
-		badTaste = new int[N];
-		for (int i = 0; i < N; i++) {
-			StringTokenizer tokenizer = new StringTokenizer(br.readLine());
-			sourTaste[i] = Integer.parseInt(tokenizer.nextToken());
-			badTaste[i] = Integer.parseInt(tokenizer.nextToken());
+		
+		n = Integer.parseInt(br.readLine());
+		ingredient = new int[n][];
+		for (int i = 0; i < n; i++) {
+			ingredient[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 		}
-		minDiff = 1000000000;
-		getBestCook(0, new boolean[N], 1, 0, 0);
-		System.out.println(minDiff);
-
+		
+		diff = Math.abs(ingredient[0][1] - ingredient[0][0]);
+		set = new boolean[n];
+		
+		getCombi(0, 0, 1, 0);
+		System.out.println(diff);
 	}
-
-	private static void getBestCook(int index, boolean[] visited, int sour, int bad, int choose) {
-		if (index == N) {
-			if (choose!=0) {
-				int diff = Math.abs(sour - bad);
-				if (diff < minDiff) {
-					minDiff = diff;
+	
+	public static void getCombi(int idx, int selectedCount, int sour, int bitter) {
+		if (idx == n) {
+			if (selectedCount > 0) {
+				if (Math.abs(sour-bitter) < diff) {
+					diff = Math.abs(sour-bitter);
 				}
 			}
 			return;
 		}
-
-		visited[index] = true;
-		getBestCook(index + 1, visited, sour * sourTaste[index], bad + badTaste[index], choose+1);
-		visited[index] = false;
-		getBestCook(index + 1, visited, sour, bad, choose);
+		set[idx] = true;
+		getCombi(idx+1, selectedCount+1, sour*ingredient[idx][0], bitter+ingredient[idx][1]);
+		set[idx] = false;
+		getCombi(idx+1, selectedCount, sour, bitter);
 	}
+
 }
