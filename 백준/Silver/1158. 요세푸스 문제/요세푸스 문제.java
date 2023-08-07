02ca95896dc05b94ce09663243f54cbf682c47ed
DASOL <66811034+DasolKang@users.noreply.github.com>
2023-08-07 17:46:51 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 504 ms, Memory: 19152 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,37 +1,31 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] input = br.readLine().split(" ");
-		int n = Integer.parseInt(input[0]);
-		int k = Integer.parseInt(input[1]);
-		
-		StringBuilder sb = new StringBuilder();
-		sb.append("<");
-		
-		int count = 0;
-		int num = 0;
-		boolean[] removed = new boolean[n+1];
-		removed[0] = true;
-		while (count < n) {
-			int move = 0;
-			while (move < k) {
-				num = num + 1 > n ?  1 : num + 1;
-				if (!removed[num]) move ++;
-			}
-			removed[num] = true;
-			
-			sb.append(num);
-			if (count + 1 < n) {
-				sb.append(", ");
-			}
-			count ++;
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int N = scanner.nextInt();
+		int K = scanner.nextInt();
+
+		Deque<Integer> deque = new ArrayDeque<>();
+		for (int i = 1; i <= N; i++) {
+			deque.add(i);
 		}
-		sb.append(">");
-		System.out.println(sb);
+		
+		List<Integer> answer = new ArrayList<>();
+		while (!deque.isEmpty()) {
+			for (int i=0;i<K-1;i++) {
+				deque.addLast(deque.pollFirst());
+			}
+			answer.add(deque.pollFirst());
+		}
+		
+		System.out.print("<");
+		for (int i=0;i<N;i++) {
+			System.out.print(answer.get(i));
+			if (i!=N-1) System.out.print(", ");
+		}
+		System.out.println(">");
 	}
 
 }
