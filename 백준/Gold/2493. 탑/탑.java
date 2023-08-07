c9a0df43dcb2ada8dd76145d28c91f3971941e47
DASOL <66811034+DasolKang@users.noreply.github.com>
2023-08-07 14:29:38 +0900

    [Gold V] Title: 탑, Time: 808 ms, Memory: 97448 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -8,24 +8,28 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		Stack<Integer> stack = new Stack<>();		
 		int[] heights = new int[n];
+		int[] answer = new int[n];
 		for (int i = 0; i < n; i++) {
 			heights[i] = Integer.parseInt(st.nextToken());
-		}
-		int index = n-1;
-
-		int[] answer = new int[n];
-		Stack<Integer> stack = new Stack<>();
-		stack.add(index--);
-		while (index > -1) {
-			while (!stack.isEmpty() && heights[index] > heights[stack.peek()]) {
-				answer[stack.pop()] = index+1;
+			
+			// 왼쪽에 있는 탑 중 더 낮은 탑은 pop
+			while (!stack.isEmpty() && heights[stack.peek()] < heights[i]) {
+				stack.pop();
 			}
-			stack.add(index--);
+			
+			if (!stack.isEmpty()) {	// 왼쪽에 현재 높이보다 더 높은 탑이 있으면
+				answer[i] = stack.peek()+1;	// 그 탑의 번호를 배열에 넣어줌
+			}
+			stack.push(i);
 		}
-		
+
+		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < n; i++) {
-			System.out.print(answer[i] + " ");
+			sb.append(answer[i] + " ");
 		}
+		System.out.println(sb);
 	}
 }
