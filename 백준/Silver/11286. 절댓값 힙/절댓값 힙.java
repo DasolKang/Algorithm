8e92f2d60cfe01abd196f292ae7160c4f42394f1
DASOL <66811034+DasolKang@users.noreply.github.com>
2023-08-09 16:12:51 +0900

    [Silver I] Title: 절댓값 힙, Time: 340 ms, Memory: 25348 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,48 +1,45 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.*;
+import java.io.*;
+
+/*
+ * 1. summary: 문제 해석
+ * 정수 x를 입력받아 배열에 저장하고, 0이 입력되면 
+ * 절댓값이 작은 값, 절댓값이 같다면 더 작은 수를 배열에서 제거후 출력
+ * 2. strategy: 풀이전략
+ * 우선순위 큐의 기준은 절댓값 -> 크기 순으로 저장한 후 차례대로 꺼낸다.
+ * 3. note: 주의할 사항(특이사항)
+ */
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		
-		PriorityQueue<Abs> queue = new PriorityQueue<>();
-		
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < n; i++) {
-			int x = Integer.parseInt(br.readLine());
-			if (x == 0) {
-				if (queue.isEmpty()) {
-					sb.append(0).append("\n");
-				} else {
-					sb.append(queue.poll().val).append("\n");
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				int n1 = Math.abs(o1);
+				int n2 = Math.abs(o2);
+				if (n1 == n2) {
+					return o1 - o2;
 				}
+				return n1 - n2;
+			}
+		});
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < N; i++) {
+			int command = Integer.parseInt(br.readLine());
+			if (command == 0) {
+				if (pq.isEmpty())
+					sb.append(0).append("\n");
+				else
+					sb.append(pq.poll()).append("\n");
 			} else {
-				queue.offer(new Abs(Math.abs(x), x));
+				pq.offer(command);
 			}
 		}
-		
-		System.out.println(sb);
+		System.out.println(sb.toString());
 	}
 
-}
-
-class Abs implements Comparable<Abs>{
-	int absVal;
-	int val;
-	public Abs(int absVal, int val) {
-		this.absVal = absVal;
-		this.val = val;
-	}
-	
-	@Override
-	public int compareTo(Abs o) {
-		if (o.absVal == this.absVal) {
-			return this.val - o.val;
-		}
-		return this.absVal - o.absVal;
-	}
-	
 }
