2f67cb5a79c37e13468e4d6e8c9da5a972284e21
DASOL <66811034+DasolKang@users.noreply.github.com>
2023-08-23 09:27:27 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 128 ms, Memory: 15664 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -4,60 +4,36 @@ import java.io.*;
 public class Main {
 
 	private static int N, numbers[], opNum[];
-	private static char[] op;
 	private static int max, min;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		input(br);
-		op = new char[N-1];
 		max = -Integer.MAX_VALUE;
 		min = Integer.MAX_VALUE;
-		solution(0, opNum[0], opNum[1], opNum[2], opNum[3]);
+		solution(1, numbers[0], opNum[0], opNum[1], opNum[2], opNum[3]);
 		System.out.println(max);
 		System.out.println(min);
 	}
 
-	private static void solution(int index, int add, int minus, int multi, int divide) {
-		if (index == N-1) {
-			int result = calculate();
+	private static void solution(int index, int result, int add, int minus, int multi, int divide) {
+		if (index == N) {
 			max = Math.max(max, result);
 			min = Math.min(min, result);
 			return;
 		}
 		if (add > 0) {
-			op[index] = '+';
-			solution(index + 1, add - 1, minus, multi, divide);
+			solution(index + 1, result+numbers[index], add - 1, minus, multi, divide);
 		}
 		if (minus > 0) {
-			op[index] = '-';
-			solution(index + 1, add, minus - 1, multi, divide);
+			solution(index + 1, result-numbers[index], add, minus - 1, multi, divide);
 		}
 		if (multi > 0) {
-			op[index] = '*';
-			solution(index + 1, add, minus, multi - 1, divide);
+			solution(index + 1, result*numbers[index], add, minus, multi - 1, divide);
 		}
 		if (divide > 0) {
-			op[index] = '/';
-			solution(index + 1, add, minus, multi, divide - 1);
+			solution(index + 1, result/numbers[index], add, minus, multi, divide - 1);
 		}
-	}
-
-	private static int calculate() {
-		int result = numbers[0];
-		for (int i = 1; i < N; i++) {
-			char oper = op[i - 1];
-			if (oper == '+') {
-				result += numbers[i];
-			} else if (oper == '-') {
-				result -= numbers[i];
-			} else if (oper == '*') {
-				result *= numbers[i];
-			} else if (oper == '/') {
-				result /= numbers[i];
-			}
-		}
-		return result;
 	}
 
 	private static void input(BufferedReader br) throws IOException {
